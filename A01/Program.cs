e11c0eaf97704c05ad6bd00ea5b99b2eac65e133
venkatesh <Venkatesh.Marimuthu@trumpf.com>
2026-07-23 14:45:32 +0530

    A01: Square of Even Numbers less than 10

---
+++ A01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+namespace A01;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Enumerable.Range(1,10).Where(x => x%2 ==0).Select(x => x*x).ToList().ForEach(Console.WriteLine);
+    }
+}
