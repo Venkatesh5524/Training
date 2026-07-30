dfb8df6dc28a334a148929d91b29efd10d66974d
venkatesh <Venkatesh.Marimuthu@trumpf.com>
2026-07-30 17:16:01 +0530

    A04: Frequency of each letters

---
+++ A04/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace A04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      string[] words = File.ReadAllLines (@"C:\Work\Training\A04\words 1.txt");
+      Dictionary<char, int> freq = new ();
+      foreach (string word in words) {
+         foreach (char c in word) {
+            if (c >= 'A' && c <= 'Z') {
+               if (!freq.ContainsKey (c)) {
+                  freq[c] = 0;
+               }
+               freq[c]++;
+            }
+         }
+      }
+      int tries = 0;
+      foreach (var ch in freq.OrderByDescending (a => a.Value)) {
+         if (tries == 7) break;
+         Console.WriteLine ($"{ch.Key} : {ch.Value}");
+         tries++;
+      }
+   }
+}
