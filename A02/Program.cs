73b0449c8de00983c18ff755c872473016340690
venkatesh <Venkatesh.Marimuthu@trumpf.com>
2026-07-31 11:13:01 +0530

    Removed Unwanted Spaces

--- A02/Program.cs
+++ A02/Program.cs
@@ -6,32 +6,27 @@ class Program
 {
     static void Main(string[] args)
     {
-      int num = new Random ().Next (1, 101); ;
-      int res;
+      int num = new Random ().Next (1, 101);
+      int res = 0;
       int count = 0;
       do {
          Console.Write ("Enter the number between 1 to 100 to be guessed: ");
          res = ReadInt ();
-         if (res < num) {
+         if (res < num)
             Console.WriteLine ("Your Guess is too Low");
-         } else if (res > num) {
-            Console.WriteLine ("Your Guess is too High");
-         }
+         else if (res > num)
+            Console.WriteLine ("Your Guess is too High");         
          count++;
       } while (res != num);
       Console.WriteLine ($"You Guessed Correctly in {count} tries.");
 
       int ReadInt () {
          for(; ; ) {
-            if(int.TryParse (Console.ReadLine (), out int result) ) {
-               if(result > 0 && result < 101) {
-                  return result;
-               }
-            } 
-            Console.WriteLine ("Enter a valid Number: ");
-            
+            if(int.TryParse (Console.ReadLine (), out int result) )
+               if(result > 0 && result < 101)
+                  return result;        
+            Console.WriteLine ("Enter a valid Number: ");            
          }         
-      }
-      
+      }      
    }
 }
