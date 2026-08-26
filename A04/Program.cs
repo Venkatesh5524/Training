6333a47dfe9068f43e373aeb38ba0122818081ba
venkatesh <Venkatesh.Marimuthu@trumpf.com>
2026-08-26 10:04:14 +0530

    Refactored letter frequency calculation and output

--- A04/Program.cs
+++ A04/Program.cs
@@ -9,17 +9,11 @@ using static System.Console;
 class Program {
    static void Main () {
       Dictionary<char, int> freq = [];
-      foreach (string word in File.ReadAllLines ("words.txt")) {
-         foreach (char c in word) {
-            if (c >= 'A' && c <= 'Z') {
-               if (freq.TryGetValue (c, out int value)) freq[c] = ++value;
-               else freq[c] = 0;
-            }
-         }
-      }
-      WriteLine ("Seven most frequently occuring letters and their occurences");
+      foreach (string word in File.ReadLines ("words.txt"))
+         foreach (char c in word.ToUpper())
+            freq[c] = (freq.TryGetValue (c, out int value)) ? ++value : 1;
       WriteLine ("Letter: Occurences");
       foreach (var ch in freq.OrderByDescending (a => a.Value).Take (7))
-         Console.WriteLine ($"{ch.Key, 3} {":", 3} {ch.Value} occurences");
+         WriteLine ($"{ch.Key, 3} {":", 3} {ch.Value}");
    }
 }
